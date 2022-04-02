30dcd5fb0c59655fae6d1d29fcb5730acf2b5780
Chelsea Lipson <chelseamlipson@gmail.com>
2022-04-02 11:13:52 -0700

    Migrated old work to new repo.

--- apps/to-do-calendar/src/app/app.tsx
+++ apps/to-do-calendar/src/app/app.tsx
@@ -16,10 +16,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Registration from './components/authentication/registration.jsx';
+import MyCalendar from './components/calendar/MyCalendar.jsx';
 
 class App extends React.Component {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  constructor (props: any) {
+  constructor(props: any) {
     super(props)
     this.state = {
       currentPage: 'home'
@@ -29,6 +30,7 @@ class App extends React.Component {
   override render() {
     return (
       <>
+        <MyCalendar />
         <Registration />
         <div />
       </>
